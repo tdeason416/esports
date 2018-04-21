15ab7f6cf380df5bb31144b9f99db4bd37758453
tdeason416 <tdeason416@gmail.com>
2018-04-21 12:41:59 -0700

    added date string formatting for API call

--- src/collect_data.py
+++ src/collect_data.py
@@ -4,26 +4,26 @@
 import json
 from pprint import pprint
 import requests
+from datetime import date, datetime, timedelta
 import os
 
 MLB_KEY = os.environ["MLB_KEY"]
 
 def get_game_schedule():
+    date_frmt = datetime.now().date().strftime('%Y/%m/%d')
     data = json.load(open('games_today.json'))
-    # games_today_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{2018/04/21}/boxscore.json?api_key={}".format(date_ftmt, MLB_KEY)
+    # games_today_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/boxscore.json?api_key={}".format(date_ftmt, MLB_KEY)
     # response = requests.get(games_today_url)
     # data = response.json()
-
     games = data['league']['games']
-
     for game in games:
         pprint(game["game"]["id"])
         pprint(game["game"]["scheduled"])
 
 
 def get_play_by_play(game_id):
-    play_by_play_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/pbp.json?api_key={}".format(game_id, MLB_KEY)
+    # play_by_play_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/pbp.json?api_key={}".format(game_id, MLB_KEY)
+    # response = requests.get(play_by_play_url)
 
-    response = requests.get(play_by_play_url)
     data = response.json()
     print(data)
