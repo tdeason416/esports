e908058e3c3ec3cc588514fe815155fd28df9989
tdeason416 <tdeason416@gmail.com>
2018-04-21 16:01:46 -0700

    added first airflow dag

---
+++ dags/load_daily_games.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import collect_data
+import os
+
+## Define the DAG object
+default_args = {
+    'concurrency' : 1,
+    'owner': 'DABGFS',
+    'email_on_failure': False,
+    'depends_on_past': False,
+    'start_date': datetime(2018, 4, 22, 00, 00),
+    'retries': 5,
+    'retry_delay': timedelta(seconds=240),
+}
+dag = DAG('daily_games', default_args=default_args, schedule_interval='30 0 * * *')
+
+update_games = PythonOperator(
+        task_id='load_daily_games',
+        python_callable=collect_data.get_game_schedule,
+        provide_context=True,
+        dag=dag)
--- src/collect_data.py
+++ src/collect_data.py
@@ -1,6 +1,3 @@
-
-
-
 import json
 from pprint import pprint
 import requests
@@ -11,19 +8,24 @@ MLB_KEY = os.environ["MLB_KEY"]
 
 def get_game_schedule():
     date_frmt = datetime.now().date().strftime('%Y/%m/%d')
-    data = json.load(open('games_today.json'))
+    ## Use this for testing
+    with open('games_today.json', 'r') as fh:
+        data = json.load(fh)
+    ## use this for prod
+    # data = json.load(open('games_today.json'))
     # games_today_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/boxscore.json?api_key={}".format(date_ftmt, MLB_KEY)
     # response = requests.get(games_today_url)
-    # data = response.json()
+    # data = response.dict()
     games = data['league']['games']
+    games_today = {}
     for game in games:
-        pprint(game["game"]["id"])
-        pprint(game["game"]["scheduled"])
+        games_today[game["game"]["id"]] = game["game"]["scheduled"]
+    with open('../data/schedule_today.json', 'wb') as wf:
+        json.dump(games_today, wf)
 
 
 def get_play_by_play(game_id):
-    # play_by_play_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/pbp.json?api_key={}".format(game_id, MLB_KEY)
-    # response = requests.get(play_by_play_url)
-
+    play_by_play_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/pbp.json?api_key={}".format(game_id, MLB_KEY)
+    response = requests.get(play_by_play_url)
     data = response.json()
     print(data)
