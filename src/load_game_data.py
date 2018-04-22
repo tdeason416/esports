e41756f89998c927fc2d797852e00208cafa803c
EC2 Default User <ec2-user@ip-172-31-6-232.us-west-2.compute.internal>
2018-04-22 04:01:41 +0000

    reorganized files to be in correct locaation

---
+++ src/load_game_data.py
@@ -0,0 +1,24 @@
+import json
+from pprint import pprint
+import requests
+from datetime import date, datetime, timedelta
+import os
+import pandas as pd
+import numpy as np
+import collect_data
+from kafka import KafkaProducer
+
+with open('/home/ec2-user/esports/schedule_today.json') as fh:
+    games_today = json.load(fh)
+
+for game_id, date in games_today.iteritems():
+    if datetime.now() > pd.Timestamp(date):
+        game_data = collect_data.get_play_by_play(game_id)
+        if game_data['game']['status'] == 'closed':
+            games_today.pop(game_id)
+        else:
+            producer = KafkaProducer(bootstrap_servers='localhost:9092')
+            producer.send('mlb_games', game_data)
+
+with open('/home/ec2-user/esports/schedule_today.json', 'wb') as fh:
+    json.dump(games_today, fh)
