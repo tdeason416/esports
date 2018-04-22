849c0656e9a3061474181b5b8a5dc65ebe735802
tdeason416 <tdeason416@gmail.com>
2018-04-21 20:46:39 -0700

    added pul_game_info.py

--- dags/load_games_daily.py
+++ dags/load_games_daily.py
@@ -17,23 +17,6 @@ args = {
 dag = DAG('daily_games', default_args=args, schedule_interval='30 0 * * *')
 
 
-def get_game_schedule():
-    date_frmt = datetime.now().date().strftime('%Y/%m/%d')
-    ## Use this for testing
-    with open('games_today.json', 'r') as fh:
-        data = json.load(fh)
-    ## use this for prod
-    # data = json.load(open('games_today.json'))
-    # games_today_url = "http://api.sportradar.us/mlb/trial/v6.5/en/games/{}/boxscore.json?api_key={}".format(date_ftmt, MLB_KEY)
-    # response = requests.get(games_today_url)
-    # data = response.dict()
-    games = data['league']['games']
-    games_today = {}
-    for game in games:
-        games_today[game["game"]["id"]] = game["game"]["scheduled"]
-    with open('../data/schedule_today.json', 'wb') as wf:
-        json.dump(games_today, wf)
-
 this_run = BashOperator(task_id='get_todays_schedule',
     bash_command="python /home/ec2-user/esports/src/find_daily_events.py", dag=dag)
 
