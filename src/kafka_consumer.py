30116633a91cec0d4df0224fb39e98bfaf4cb8db
EC2 Default User <ec2-user@ip-172-31-6-232.us-west-2.compute.internal>
2018-04-23 00:37:13 +0000

    created kafka consumer running in nohup

---
+++ src/kafka_consumer.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer
+import json
+import ast
+import pymongo
+from pymongo import MongoClient
+
+KAFKA_TOPIC_NAME='mlb_games'
+MONGO_DB_NAME='mlb_games_db'
+
+def insert_record(record):
+    client = MongoClient('localhost', 27017)
+    db = client[MONGO_DB_NAME]
+    record_id = db.mlb_collection.insert_one(record).inserted_id
+    print(record_id)
+    
+#consumer = KafkaConsumer(bootstrap_servers='localhost:9092', auto_offset_reset='earliest', consumer_timeout_ms=1000)
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092', consumer_timeout_ms=1000)
+consumer.subscribe([KAFKA_TOPIC_NAME])
+
+while True:
+    for message in consumer:
+        data = ast.literal_eval(message.value)
+        print(data)
+        insert_record(data)
+
